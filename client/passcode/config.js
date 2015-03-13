8b6f4a052c28c06e31c00bc6485de065dbf919b7
carlosdubus <carlos.garciadm@gmail.com>
2015-03-13 18:13:28 -0400

    refactor controllers

--- client/passcode/config.js
+++ client/passcode/config.js
@@ -1,163 +1,98 @@
 module.exports = function(module) {
+    /**
+     * RestangularProvider
+     */
     module.config(function(RestangularProvider) {
 	RestangularProvider.setBaseUrl('/api');
-	//RestangularProvider.setRequestSuffix('.json');
     })
+    /**
+     * Config states
+     */
     .config(['$httpProvider', '$stateProvider', '$urlRouterProvider', '$locationProvider', function($httpProvider, $stateProvider, $urlRouterProvider, $locationProvider) {
-	    $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
+	$httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
 
-	    $locationProvider.hashPrefix('!');
-	    // For any unmatched url, redirect to /state1
-	    $urlRouterProvider.otherwise("/");
+	$locationProvider.hashPrefix('!');
+	// For any unmatched url, redirect to /state1
+	$urlRouterProvider.otherwise("/");
 
-	    // Now set up the states
-	    $stateProvider
-	    .state('index', {
-		url: "/?confirmSent",
-		templateUrl: "index.html",
-		controller: function($scope, $stateParams, modal, $http, $modal, Events) {
-
-		    if ($stateParams.confirmSent) {
-			modal.confirmEmail($stateParams.confirmSent);
-		    }
-
-		    $scope.events = Events.getList().$object;
-
-
+	// Now set up the states
+	$stateProvider
+	.state('index', {
+	    url: "/?confirmSent",
+	    templateUrl: "index.html",
+	    controller: "IndexCtrl"
+	})
+	.state("login", {
+	    url: "/login?redirect&needToLogin",
+	    templateUrl: "login.html",
+	    controller: 'LoginCtrl',
+	    beforeFilter: 'guest'
+	})
+	.state("signup", {
+	    url: "/signup",
+	    templateUrl: "signup.html",
+	    beforeFilter: 'guest'
+	})
+	.state('events', {
+	    url: "/events?confirmed",
+	    templateUrl: "events.html",
+	    controller: 'EventsCtrl'
+	})
+	.state("events:view", {
+	    url: "/events/:eventId",
+	    templateUrl: 'events/view.html',
+	    resolve: {
+		event: function(Events, $stateParams) {
+		    return Events.one($stateParams.eventId).get();
 		}
-	    })
-	    .state("login", {
-		url: "/login?redirect&needToLogin",
-		templateUrl: "login.html",
-		controller: 'LoginCtrl',
-		beforeFilter: 'guest'
-	    })
-	    .state("signup", {
-		url: "/signup",
-		templateUrl: "signup.html",
-		beforeFilter: 'guest'
-	    })
-	    .state('events', {
-		url: "/events?confirmed",
-		templateUrl: "events.html",
-		controller: function($scope, $stateParams, modal, $http, $modal, Events) {
-
-		    if ($stateParams.confirmed) {
-			modal.open('confirmed');
-		    }
-
-		    $scope.events = Events.getList().$object;
-
-
+	    },
+	    controller: 'ViewEventController'
+	})
+	.state('my-tickets', {
+	    url: "/my-tickets",
+	    templateUrl: "my-tickets.html",
+	    resolve: {
+		tickets: function(Tickets) {
+		    return Tickets().getList();
 		}
-	    })
-	    .state("events:view", {
-		url: "/events/:eventId",
-		templateUrl: 'events/view.html',
-		resolve: {
-		    event: function(Events, $stateParams) {
-			return Events.one($stateParams.eventId).get();
-		    }
-		},
-		controller: 'ViewEventController'
-	    })
-	    .state('my-tickets', {
-		url: "/my-tickets",
-		templateUrl: "my-tickets.html",
-		resolve: {
-		    tickets: function(Tickets) {
-			return Tickets().getList();
-		    }
-		},
-		controller: 'MyTicketsController',
-		beforeFilter: 'auth'
-	    })
-	    .state("my-tickets:view", {
-		url: "/my-tickets/:ticketToken",
-		templateUrl: "my-tickets/view.html",
-		controller: 'MyTicketsViewController',
-		beforeFilter: 'auth',
-		resolve: {
-		    ticket: function(Tickets, $stateParams) {
-			return Tickets().one($stateParams.ticketToken).get();
-		    }
+	    },
+	    controller: 'MyTicketsController',
+	    beforeFilter: 'auth'
+	})
+	.state("my-tickets:view", {
+	    url: "/my-tickets/:ticketToken",
+	    templateUrl: "my-tickets/view.html",
+	    controller: 'MyTicketsViewController',
+	    beforeFilter: 'auth',
+	    resolve: {
+		ticket: function(Tickets, $stateParams) {
+		    return Tickets().one($stateParams.ticketToken).get();
 		}
-	    })
-	    .state("error", {
-		url: "/error?msg",
-		templateUrl: "error.html",
-		controller: function($scope, $stateParams) {
-		    $scope.msg = $stateParams.msg;
+	    }
+	})
+	.state("error", {
+	    url: "/error?msg",
+	    templateUrl: "error.html",
+	    controller: function($scope, $stateParams) {
+		$scope.msg = $stateParams.msg;
+	    }
+	})
+	.state("admin/select-event", {
+	    url: "/admin/select-event?redirect&needEventKey",
+	    templateUrl: "admin/select-event.html",
+	    beforeFilter: "auth|admin",
+	    resolve: {
+		events: function(Restangular) {
+		    return Restangular.all('admin/events').getList();
 		}
-	    })
-	    .state("admin/select-event", {
-		url: "/admin/select-event?redirect&needEventKey",
-		templateUrl: "admin/select-event.html",
-		beforeFilter: "auth|admin",
-		resolve: {
-		    events: function(Restangular) {
-			return Restangular.all('admin/events').getList();
-		    }
-		},
-		controller: function($scope, $stateParams, AdminSession, $location, events) {
-		    $scope.needEventKey = $stateParams.needEventKey;
-
-		    $scope.events = events;
-		    $scope.currentEventKey = AdminSession['event-key'];
-
-		    $scope.selectEvent = function(eventKey) {
-			$scope.saved = false;
-			$scope.error = false;
-
-			if (eventKey) {
-			    $scope.saving = true;
-			    AdminSession.set('event-key', eventKey).then(function() {
-				$scope.saved = true;
-				$scope.saving = false;
-				$scope.currentEventKey = eventKey;
-				if ($stateParams.redirect) {
-				    $location.url($stateParams.redirect);
-				}
-			    }, function(response) {
-				$scope.saving = false;
-				$scope.error = response.data.error;
-			    });
-			}
-			else {
-			    $scope.error = {
-				message: "Event has no key."
-			    };
-			}
-		    };
-		}
-	    })
-	    .state("admin/use-ticket", {
-		url: "/admin/use-ticket/:ticketToken",
-		templateUrl: "admin/use-ticket.html",
-		beforeFilter: "auth|admin|eventKey",
-		controller: function(AdminSession, $stateParams, Restangular, $scope, $state) {
-
-		    $scope.error = false;
-		    $scope.success = false;
-
-		    $scope.changeEvent = function() {
-			var redirect = $state.href($state.current.name, $stateParams).replace('#!', '');
-
-			$state.go('admin/select-event', {
-			    redirect: redirect
-			});
-		    };
-
-		    Restangular.one('tickets', $stateParams.ticketToken).customPUT({
-			used: true,
-			eventKey: AdminSession['event-key']
-		    })
-		    .then(function() {
-			$scope.success = true;
-		    }, function(response) {
-			$scope.error = response.data.error;
-		    });
-		}
-	    })
-	}])
+	    },
+	    controller: 'SelectEventCtrl'
+	})
+	.state("admin/use-ticket", {
+	    url: "/admin/use-ticket/:ticketToken",
+	    templateUrl: "admin/use-ticket.html",
+	    beforeFilter: "auth|admin|eventKey",
+	    controller: "UseTicketCtrl"
+	});
+    }]);
 };
--- client/passcode/controllers.js
+++ client/passcode/controllers.js
@@ -1,52 +1,59 @@
 module.exports = function(module) {
-    
-    module.controller('ViewEventController', function($scope, event, Auth, Tickets, $state) {
+    /**
+     * IndexCtrl
+     */
+    module.controller('IndexCtrl',function($scope, $stateParams, modal, $http, $modal, Events) {
+	if ($stateParams.confirmSent) {
+	    modal.confirmEmail($stateParams.confirmSent);
+	}
+
+	$scope.events = Events.getList().$object;
+    })
+    /**
+     * ViewEventController
+     */
+    .controller('ViewEventController', function($scope, event, Auth, Tickets, $state) {
 	$scope.event = event;
 
 	$scope.buyTicket = function() {
-
 	    $scope.buying = true;
 	    $scope.hasError = false;
 	    Tickets().post({
 		eventId: event.id
 	    })
-		    .then(function(res) {
-			$state.go('my-tickets:view', {
-			    ticketToken: res.token
-			});
-		    }, function() {
-			$scope.hasError = true;
-			$scope.buying = false;
-		    });
-
-
+	    .then(function(res) {
+		$state.go('my-tickets:view', {
+		    ticketToken: res.token
+		});
+	    }, function() {
+		$scope.hasError = true;
+		$scope.buying = false;
+	    });
 	};
     })
+    /**
+     * MyTicketsController
+     */
     .controller('MyTicketsController', function($scope, $state, tickets, Auth) {
-
 	if (!Auth.user) {
 	    return $state.go('index');
 	}
-
 	$scope.tickets = tickets;
-
-
     })
+    /**
+     * MyTicketsViewController
+     */
     .controller('MyTicketsViewController', function($scope, ticket) {
 	$scope.ticket = ticket;
     })
     /**
      * MainCtrl
-     * @param {type} loginModal
-     * @returns {undefined}
      */
     .controller('MainCtrl', function() {
 
     })
-
     /**
      * LoginCtrl
-     * @returns {undefined}
      */
     .controller('LoginCtrl', function($scope, $http, $window, $state, $stateParams, Auth, $location) {
 
@@ -56,8 +63,7 @@ module.exports = function(module) {
 	    if ($event) {
 		$event.preventDefault();
 	    }
-
-
+	    
 	    form.$submitted = true;
 	    form.$setValidity('validCredentials', true);
 
@@ -78,9 +84,7 @@ module.exports = function(module) {
 			if ($stateParams.redirect) {
 			    redirect = $stateParams.redirect;
 			}
-
-
-
+			
 			$location.url(redirect);
 		    }
 
@@ -88,8 +92,11 @@ module.exports = function(module) {
 
 		});
 	    }
-	}
+	};
     })
+    /**
+     * SignUpCtrl
+     */
     .controller('SignUpCtrl', function($scope, $http, modal, Users, $state) {
 
 	$scope.data = {};
@@ -101,36 +108,101 @@ module.exports = function(module) {
 
 	$scope.invalid = function(form, name) {
 	    return $scope.dirty(form, name) && form[name].$invalid;
-	}
+	};
 
 	$scope.error = function(name, error) {
 	    return $scope.dirty($scope.form, name) && $scope.form[name].$error[error];
-	}
+	};
 
 	$scope.submit = function(form, $event) {
 	    if ($event) {
 		$event.preventDefault();
 	    }
 
-
 	    form.$submitted = true;
-
 	    if (form.$valid) {
-
 		form.$submitting = true;
-
 		Users
-			.post($scope.data)
-			.then(function(res) {
-			    form.$submitting = false;
-			    $state.go('index', {
-				confirmSent: $scope.data.email
-			    });
-			}, function(res) {
-			    form.$submitting = false;
-			    $scope.hasError = true;
-			});
+		.post($scope.data)
+		.then(function(res) {
+		    form.$submitting = false;
+		    $state.go('index', {
+			confirmSent: $scope.data.email
+		    });
+		}, function(res) {
+		    form.$submitting = false;
+		    $scope.hasError = true;
+		});
 	    }
 	};
-    });
+    })
+    /**
+     * SelectEventCtrl
+     */
+    .controller('SelectEventCtrl', function($scope, $stateParams, AdminSession, $location, events) {
+	$scope.needEventKey = $stateParams.needEventKey;
+
+	$scope.events = events;
+	$scope.currentEventKey = AdminSession['event-key'];
+
+	$scope.selectEvent = function(eventKey) {
+	    $scope.saved = false;
+	    $scope.error = false;
+
+	    if (eventKey) {
+		$scope.saving = true;
+		AdminSession.set('event-key', eventKey).then(function() {
+		    $scope.saved = true;
+		    $scope.saving = false;
+		    $scope.currentEventKey = eventKey;
+		    if ($stateParams.redirect) {
+			$location.url($stateParams.redirect);
+		    }
+		}, function(response) {
+		    $scope.saving = false;
+		    $scope.error = response.data.error;
+		});
+	    }
+	    else {
+		$scope.error = {
+		    message: "Event has no key."
+		};
+	    }
+	};
+    })
+    /**
+     * UseTicketCtrl
+     */
+    .controller('UseTicketCtrl', function(AdminSession, $stateParams, Restangular, $scope, $state) {
+	$scope.error = false;
+	$scope.success = false;
+
+	$scope.changeEvent = function() {
+	    var redirect = $state.href($state.current.name, $stateParams).replace('#!', '');
+
+	    $state.go('admin/select-event', {
+		redirect: redirect
+	    });
+	};
+
+	Restangular.one('tickets', $stateParams.ticketToken).customPUT({
+	    used: true,
+	    eventKey: AdminSession['event-key']
+	})
+	.then(function() {
+	    $scope.success = true;
+	}, function(response) {
+	    $scope.error = response.data.error;
+	});
+    })
+    /**
+     * EventsCtrl
+     */
+    .controller("EventsCtrl",function($scope, $stateParams, modal, $http, $modal, Events) {
+	if ($stateParams.confirmed) {
+	    modal.open('confirmed');
+	}
+
+	$scope.events = Events.getList().$object;
+    })
 };
