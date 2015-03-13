eed1bd145baea1b33a06d44420fe35d7b5a12cad
carlosdubus <carlos.garciadm@gmail.com>
2015-03-13 18:31:19 -0400

    mobile navbar issue

--- client/extra.js
+++ client/extra.js
@@ -1,5 +1,7 @@
 $(function(){
-    // hotfix for navbar
-    // on mobile, navbar was not closing when chaging states, so I added this jquery code, I know this is not the place bla bla...
-    $('.navbar-collapse').removeClass('in');
+	$(document).click(function(){
+	    // hotfix for navbar
+	    // on mobile, navbar was not closing when chaging states, so I added this jquery code, I know this is not the place bla bla...
+	    $('.navbar-collapse').removeClass('in');
+	});
 });
